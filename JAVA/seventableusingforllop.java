d351ffccb94b4a0ce3b7496776c9f5788dd8f64c
keerth-i2k6 <keerthanajannu331@gmail.com>
2025-12-11 12:11:37 +0530

    Add files via upload

---
+++ JAVA/seventableusingforllop.java
@@ -0,0 +1,9 @@
+public class seventableusingforllop {
+    public static void main(String[] args){
+        for(int i=1;i<=10;i++){
+            int result=7*i;
+            System.out.println("7"+ "*"+ i + "=" + result);
+        }
+    }   
+    
+}
